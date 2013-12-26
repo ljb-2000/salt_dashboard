3fd6cbe20104e2780735a288733dc16fb4f52e07
tanyewei <tanyewei@gmail.com>
2013-12-26 09:22:19 +0800

    fix command

--- app/views.py
+++ app/views.py
@@ -16,12 +16,13 @@ from app.forms import SaltForm, TestForm, MultiCheckboxField, CommandForm
 from app.util import AESdecrypt
 from app import db
 from jinja2 import Markup
+from flask.ext.admin.contrib.fileadmin import FileAdmin
 
 
 class HostModelView(sqla.ModelView):
     #column_exclude_list = ('grains')
     #form_overrides = dict(name=FileField)
-    column_filters = ('group.name', )
+    column_filters = (('group.name'), )
     column_searchable_list = ('name', Host.name)
     from app.models import Grains
 
@@ -108,7 +109,7 @@ class JobModelView(sqla.ModelView):
 
 class UserModelView(sqla.ModelView):
     column_searchable_list = ('login', User.login)
-    column_filters = ('login', User.login)
+    column_filters = (User.login, )
     column_exclude_list = ('password')
     column_labels = dict(login=u'登录名', email=u'邮件地址', password=u'密码')
 
@@ -128,8 +129,11 @@ class SaltView(BaseView):
     def index(self):
         form = CommandForm(request.form)
         if helpers.validate_form_on_submit(form):
-            ret = client.cmd_async(tgt=form.tgt.data, fun=form.fun.data, arg=form.args.data.split(';'),
-                                   expr_form='compound', ret='http_api')
+            if form.args.data != "":
+                ret = client.cmd_async(tgt=form.tgt.data, fun=form.fun.data, arg=form.args.data.split(';'),
+                                       expr_form='compound', ret='http_api')
+            else:
+                ret = client.cmd_async(tgt=form.tgt.data, fun=form.fun.data, expr_form='compound', ret='http_api')
             flash(str(ret))
         jobs = Returner.query.order_by(Returner.id.desc()).limit(10).all()
         return self.render('saltstack/command.html', form=form, jobs=jobs)
@@ -165,3 +169,79 @@ def ret():
         db.session.commit()
         minion.write_ret(ret=data)
     return 'ok'
+
+
+import os
+from flask.ext.admin.contrib.fileadmin import EditForm
+from flask.ext.admin.babel import gettext, lazy_gettext
+
+
+class FileManage(FileAdmin):
+    allowed_extensions = ('swf', 'jpg', 'gif', 'png')
+    editable_extensions = ('md', 'html', 'txt')
+
+    @expose('/edit/', methods=('GET', 'POST'))
+    def edit(self):
+        """
+            Edit view method
+        """
+        path = request.args.getlist('path')
+        next_url = None
+        if not path:
+            return redirect(url_for('.index'))
+
+        if len(path) > 1:
+            next_url = url_for('.edit', path=path[1:])
+        path = path[0]
+
+        base_path, full_path, path = self._normalize_path(path)
+
+        if not self.is_accessible_path(path):
+            flash(gettext(gettext('Permission denied.')))
+            return redirect(self._get_dir_url('.index'))
+
+        dir_url = self._get_dir_url('.index', os.path.dirname(path))
+        next_url = next_url or dir_url
+
+        from app.forms import CKEditForm
+
+        form = EditForm(helpers.get_form_data())
+        error = False
+
+        if helpers.validate_form_on_submit(form):
+            form.process(request.form, content='')
+            if form.validate():
+                try:
+                    with open(full_path, 'w') as f:
+                        f.write(request.form['content'].encode('UTF-8'))
+                except IOError:
+                    flash(gettext("Error saving changes to %(name)s.", name=path), 'error')
+                    error = True
+                else:
+                    self.on_edit_file(full_path, path)
+                    flash(gettext("Changes to %(name)s saved successfully.", name=path))
+                    return redirect(next_url)
+        else:
+            try:
+                with open(full_path, 'r') as f:
+                    content = f.read()
+            except IOError:
+                flash(gettext("Error reading %(name)s.", name=path), 'error')
+                error = True
+            except:
+                flash(gettext("Unexpected error while reading from %(name)s", name=path), 'error')
+                error = True
+            else:
+                try:
+                    content = content.decode('utf8')
+                except UnicodeDecodeError:
+                    flash(gettext("Cannot edit %(name)s.", name=path), 'error')
+                    error = True
+                except:
+                    flash(gettext("Unexpected error while reading from %(name)s", name=path), 'error')
+                    error = True
+                else:
+                    form.content.data = content
+
+        return self.render(self.edit_template, dir_url=dir_url, path=path,
+                           form=form, error=error)
